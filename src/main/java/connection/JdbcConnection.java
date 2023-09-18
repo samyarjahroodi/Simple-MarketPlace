146bb05b5a8c308c436788897e85408f9c967093
samyarjahroodi <samyarghasemjahroodi@gmail.com>
2023-09-18 22:41:58 +0330

    jdbc connection fully completed

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,5 +1,25 @@
 package connection;
 
-public class JdbcConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class JdbcConnection {
+    private Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+            "postgres", "1381Iran");
+
+    public JdbcConnection() throws SQLException {
+    }
+
+    public JdbcConnection(Connection connection) throws SQLException {
+        this.connection = connection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
 }
