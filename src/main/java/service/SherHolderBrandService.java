b85883a2d3e951eff1505ba7adccc15a99e2adf4
samyarjahroodi <samyarghasemjahroodi@gmail.com>
2023-09-25 23:38:57 +0330

    some changes that was applied

--- src/main/java/service/SherHolderBrandService.java
+++ src/main/java/service/SherHolderBrandService.java
@@ -1,13 +1,6 @@
 package service;
 
-import model.Brand;
-import model.ShareHolder;
-import repository.ShareHolderRepository;
-import validation.Validation;
-
-import java.sql.SQLException;
-import java.util.Scanner;
-
 public class SherHolderBrandService {
+
 
 }
