17b2b4c2bd4087fe1630dea1947c3aefb6c17041
samyarjahroodi <samyarghasemjahroodi@gmail.com>
2023-09-25 23:40:46 +0330

    username exist method added

--- src/main/java/service/UsersService.java
+++ src/main/java/service/UsersService.java
@@ -25,7 +25,7 @@ public class UsersService {
         System.out.println("Enter your email");
         users.setEmail(validation.isValidEmail());
         System.out.println("Enter your password");
-        users.setPasswordOfUsers(validation.isValidPassword());
+        users.setPasswordOfUsers(validation.isValidPasswordForRegistration());
         int result = usersRepository.saveUser(users);
         if (result != 0) {
             System.out.println("SUCCESSFULLY ADDED TO THE DATABASE");
@@ -36,17 +36,32 @@ public class UsersService {
 
     public boolean login() throws SQLException {
         Users users = new Users();
-        System.out.println("Enter your username");
-        users.setUsername(scanner.next());
+        boolean usernameExists = false;
+
+        while (!usernameExists) {
+            System.out.println("Enter your username");
+            String username = scanner.next();
+            usernameExists = usersRepository.doesUserExist(username);
+
+            if (usernameExists) {
+                System.out.println("YOUR USERNAME EXISTS");
+                users.setUsername(username); // Set the username in the Users object
+            } else {
+                System.out.println("THERE IS NO MATCH IN DATABASE");
+            }
+        }
+
         System.out.println("Enter your password");
-        users.setPasswordOfUsers(validation.isValidPassword());
+        users.setPasswordOfUsers(scanner.next());
         boolean loginSuccessful = usersRepository.login(users);
+
         if (loginSuccessful) {
             System.out.println("SUCCESSFULLY LOGGED IN");
         } else {
-            System.out.println("THERE IS A PROBLEM WITH EITHER OF YOUR USERNAME OR PASSWORD");
+            System.out.println("THERE IS A PROBLEM WITH EITHER YOUR USERNAME OR PASSWORD");
             System.exit(0);
         }
+
         return true;
     }
 }
