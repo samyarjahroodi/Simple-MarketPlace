83b5493f416e22a5a86653dc95a0807efd85e0a7
samyarjahroodi <samyarghasemjahroodi@gmail.com>
2023-09-25 23:35:01 +0330

    show brand and exist method added

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -5,6 +5,7 @@ import model.Brand;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @SuppressWarnings("unused")
@@ -31,14 +32,33 @@ public class BrandRepository {
         return preparedStatement.executeUpdate();
     }
 
-    public int edit(int id) throws SQLException {
+    public int edit(Brand brand) throws SQLException {
         String edit = "UPDATE brand set nameOfBrand=?,website=?,description=? where id=? ";
-        Brand brand = new Brand();
         PreparedStatement preparedStatement = connection.prepareStatement(edit);
         preparedStatement.setString(1, brand.getNameOfBrand());
         preparedStatement.setString(2, brand.getWebsite());
         preparedStatement.setString(3, brand.getDescription());
-        preparedStatement.setInt(4, id);
+        preparedStatement.setInt(4, brand.getId());
         return preparedStatement.executeUpdate();
+    }
+
+    public void showBrands() throws SQLException {
+        String show = "SELECT * from brand";
+        PreparedStatement preparedStatement = connection.prepareStatement(show);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String nameOfBrand = resultSet.getString("nameOfBrand");
+            System.out.println("Brand ID: " + id);
+            System.out.println("Name of Brand: " + nameOfBrand);
+        }
+    }
+
+    public boolean exist(int id) throws SQLException {
+        String exist = "SELECT 1 FROM brand WHERE id=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(exist);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        return resultSet.next();
     }
 }
